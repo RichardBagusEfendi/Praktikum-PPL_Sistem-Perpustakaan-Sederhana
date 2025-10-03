ca232ff086d86078f723cc28a945174b65a92e80
Richard Bagus Efendi <rbagusefendi@gmail.com>
2025-10-03 22:19:09 +0700

    create file

--- src/main/java/com/praktikum/testing/repository/RepositoriBuku.java
+++ src/main/java/com/praktikum/testing/repository/RepositoriBuku.java
@@ -1,4 +1,15 @@
 package com.praktikum.testing.repository;
 
-public class RepositoriBuku {
+import com.praktikum.testing.model.Buku;
+import java.util.List;
+import java.util.Optional;
+
+public interface RepositoriBuku {
+    boolean simpan(Buku buku);
+    Optional<Buku> cariByIsbn(String isbn);
+    List<Buku> cariByJudul(String judul);
+    List<Buku> cariByPengarang(String pengarang);
+    boolean hapus(String isbn);
+    boolean updateJumlahTersedia(String isbn, int jumlahTersediaBaru);
+    List<Buku> cariSemua();
 }
